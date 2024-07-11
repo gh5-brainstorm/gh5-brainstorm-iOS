33bfe73ba845a2961a7c02254637fb59768fa02a
Danny <danny.santoso@thecarousell.com>
2024-07-11 19:08:01 +0700

    Initial Commit

---
+++ gh5-brainstrom-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  gh5-brainstrom-ios
+//
+//  Created by danny santoso on 7/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
