ef93f95ddd18163acb7cec51aa5c525338f15abf
Danny <danny.santoso@thecarousell.com>
2024-07-12 18:11:51 +0700

    create watermark for duplicate or plagiarsm

---
+++ gh5-brainstrom-ios/ImageGridCollectionView.swift
@@ -0,0 +1,92 @@
+//
+//  ImageGridCollectionView.swift
+//  gh5-brainstrom-ios
+//
+//  Created by danny santoso on 7/12/24.
+//
+
+import UIKit
+
+class ImageGridViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    var collectionView: UICollectionView!
+    
+    var images: [String] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let layout = UICollectionViewFlowLayout()
+        collectionView = UICollectionView(frame: self.view.frame, collectionViewLayout: layout)
+        collectionView.backgroundColor = .white
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.register(ImageCell.self, forCellWithReuseIdentifier: "ImageCell")
+        
+        self.view.addSubview(collectionView)
+    }
+    
+    // MARK: - UICollectionViewDataSource Methods
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return images.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ImageCell", for: indexPath) as! ImageCell
+        cell.configure(with: images[indexPath.item])
+        return cell
+    }
+    
+    // MARK: - UICollectionViewDelegateFlowLayout Methods
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let collectionViewWidth = collectionView.frame.size.width
+        let itemWidth = collectionViewWidth / 3
+        return CGSize(width: itemWidth, height: itemWidth)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+    }
+}
+
+import UIKit
+import SDWebImage
+
+class ImageCell: UICollectionViewCell {
+    
+    var imageView: UIImageView!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        imageView = UIImageView(frame: self.contentView.frame)
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        contentView.addSubview(imageView)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        imageView.frame = contentView.bounds
+    }
+    
+    func configure(with urlString: String) {
+        guard let url = URL(string: urlString) else { return }
+        imageView.sd_setImage(with: url, placeholderImage: UIImage(named: "placeholder"))
+    }
+}
+
--- gh5-brainstrom-ios/ScanViewController.swift
+++ gh5-brainstrom-ios/ScanViewController.swift
@@ -11,6 +11,12 @@ class ScanViewController: UIViewController {
     
     var image: UIImage?
 
+    @IBOutlet weak var noteLabel: UILabel! {
+        didSet {
+            noteLabel.text = "This Image is plagiarism"
+            noteLabel.textColor = UIColor.red
+        }
+    }
     @IBOutlet weak var frameImageView: UIImageView!
     @IBOutlet weak var photoImageView: UIImageView! {
         didSet {
@@ -18,20 +24,78 @@ class ScanViewController: UIViewController {
         }
     }
     
-    @IBOutlet weak var proceedButton: UIButton! {
+    @IBOutlet weak var similarityButton: UIButton! {
         didSet {
-            proceedButton.backgroundColor = UIColor(rgb: 0x1B75BB)
-            proceedButton.setTitle("Scan", for: .normal)
-            proceedButton.setTitleColor(.white, for: .normal)
-            proceedButton.layer.cornerRadius = 10
+            similarityButton.backgroundColor = UIColor(rgb: 0x1B75BB)
+            similarityButton.setTitle("Show Simillar Arts", for: .normal)
+            similarityButton.setTitleColor(.white, for: .normal)
+            similarityButton.layer.cornerRadius = 10
         }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         photoImageView.image = image
+        
+        // Add diagonal label view to frameImageView
+        let diagonalLabelView = DiagonalLabelView(frame: photoImageView.bounds)
+        diagonalLabelView.configure(text: "THIS IMAGE 100% PLAGIARISM")
+        diagonalLabelView.translatesAutoresizingMaskIntoConstraints = false
+        photoImageView.addSubview(diagonalLabelView)
+        
+        // Constraints for diagonalLabelView
+        NSLayoutConstraint.activate([
+            diagonalLabelView.leadingAnchor.constraint(equalTo: photoImageView.leadingAnchor),
+            diagonalLabelView.trailingAnchor.constraint(equalTo: frameImageView.trailingAnchor),
+            diagonalLabelView.topAnchor.constraint(equalTo: photoImageView.topAnchor),
+            diagonalLabelView.bottomAnchor.constraint(equalTo: photoImageView.bottomAnchor)
+        ])
     }
 
-    @IBAction func didTapProceedButton(_ sender: Any) {
+    @IBAction func didTapShowSimillarity(_ sender: Any) {
+        let imageGridVC = ImageGridViewController()
+        imageGridVC.images = [
+            "https://rukminim2.flixcart.com/image/850/1000/l1mh7rk0/poster/b/s/j/medium-famous-cartoon-poster-for-kids-shinchan-cartoon-wall-original-imagd5fcasqbqfcg.jpeg?q=20&crop=false",
+            "https://rukminim2.flixcart.com/image/850/1000/l1mh7rk0/poster/b/s/j/medium-famous-cartoon-poster-for-kids-shinchan-cartoon-wall-original-imagd5fcasqbqfcg.jpeg?q=20&crop=false"]
+        self.present(imageGridVC, animated: true, completion: nil)
+    }
+}
+
+import UIKit
+
+class DiagonalLabelView: UIView {
+    private let label = UILabel()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupView()
+    }
+    
+    private func setupView() {
+        // Set up the label
+        label.backgroundColor = UIColor.red
+        label.textColor = .white
+        label.textAlignment = .center
+        label.font = UIFont.boldSystemFont(ofSize: 16)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.transform = CGAffineTransform(rotationAngle: -45 * (.pi / 180)) // Rotate the label by -45 degrees
+        addSubview(label)
+        
+        // Add constraints
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: centerYAnchor),
+            label.widthAnchor.constraint(equalTo: widthAnchor, constant: sqrt(bounds.width * bounds.width + bounds.height * bounds.height)),
+            label.heightAnchor.constraint(equalToConstant: 20) // Set the label's height to 20
+        ])
+    }
+    
+    func configure(text: String) {
+        label.text = text
     }
 }
